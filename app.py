1a2c01518b3a149c4a2bf28dbbb09d80436e4c5e
ANSHUL KUMAR <anshulkumarchauhan25@gmail.com>
2026-04-10 10:48:53 +0530

    Refactor app.py for model and title updates
    Updated model file from 'best.onnx' to 'best.pt' and changed title.

--- app.py
+++ app.py
@@ -3,29 +3,28 @@ from ultralytics import YOLO
 from PIL import Image
 import numpy as np
 
-# Page Configuration
+#Page Configuration
 st.set_page_config(page_title="Neer-Nirikshan AI Demo", layout="wide")
 
-st.title("🚧 Neer-Nirikshan: Pothole & Waterlogging Detection")
+st.title("🚧 PitWatch: Pothole & Waterlogging Detection")
 st.write("Upload a road image to see the AI identify hazards in real-time.")
 
-# 1. Load your ONNX model
-# Ensure 'best.onnx' is in the same folder as this script
+#Loading our model
 @st.cache_resource
 def load_model():
-    model = YOLO("best.onnx", task="detect")
+    model = YOLO("best.pt", task="detect")
     return model
 
 model = load_model()
 
-# 2. Sidebar for settings
+#Sidebar for settings
 conf_threshold = st.sidebar.slider("Confidence Threshold", 0.0, 1.0, 0.45)
 
-# 3. File Uploader
+#File Uploader
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Convert uploaded file to OpenCV format
+    #Converting uploaded file to OpenCV format
     image = Image.open(uploaded_file)
     img_array = np.array(image)
     
@@ -35,13 +34,13 @@ if uploaded_file is not None:
         st.image(image, caption="Original Image", use_container_width=True)
         
     with col2:
-        # Run AI Inference
+        #running our prediction
         results = model.predict(source=img_array, conf=conf_threshold)
         
-        # Plot the results (bounding boxes) on the image
+        #Plotting the results (bounding boxes) on the uploaded image
         annotated_img = results[0].plot()
         
         st.image(annotated_img, caption="AI Detection Result", use_container_width=True)
         
-        # Display detected hazards count
+        #Display detected hazards count
         st.success(f"Detected {len(results[0])} hazards on this road.")
